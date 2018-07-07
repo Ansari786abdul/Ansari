ce5b445b48a32f1f08509084d58bc2c6037a8475
Ansari786abdul <40906663+Ansari786abdul@users.noreply.github.com>
2018-07-07 10:24:03 +0530

    Update 2. Even or odd.py
    nothing

--- 2. Even or odd.py
+++ 2. Even or odd.py
@@ -1,6 +1,7 @@
-x=int(input("Enter an integer:"))
-if ((x%2)==0):
-    print("number is even")
-
+num=int(raw_input())
+if (num<0):
+	print("num is invalid")
+elif (num%2==0):
+	print("number is even")
 else:
-    print("number is odd")
+	print("number is odd")
